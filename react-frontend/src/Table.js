c84af3e66b3e94c38831471c162688e0a679595a
joanna-chou <joanna.chouu@gmail.com>
2023-09-26 19:40:38 -0700

    added remove button

--- react-frontend/src/Table.js
+++ react-frontend/src/Table.js
@@ -6,6 +6,7 @@ function TableHeader() {
         <tr>
           <th>Name</th>
           <th>Job</th>
+          <th>Remove</th>
         </tr>
       </thead>
     );
@@ -17,6 +18,12 @@ function TableHeader() {
         <tr key={index}>
             <td>{row.name}</td>
             <td>{row.job}</td>
+            <td>
+            <button onClick={() => 
+				props.removeCharacter(index)}>
+				Delete
+			</button>
+            </td>
         </tr>
         );
     }
@@ -33,7 +40,8 @@ function TableHeader() {
       return (
         <table>
           <TableHeader />
-          <TableBody characterData={props.characterData}/>
+          <TableBody characterData={props.characterData}
+                    removeCharacter = {props.removeCharacter}/>
         </table>
       );
   }
