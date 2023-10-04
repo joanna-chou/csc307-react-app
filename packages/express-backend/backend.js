08b54c9d67d352761016bb3c18113991c0b5b571
joanna-chou <joanna.chouu@gmail.com>
2023-10-03 18:05:44 -0700

    added functions for get user & id

--- packages/express-backend/backend.js
+++ packages/express-backend/backend.js
@@ -37,6 +37,36 @@ const users = {
         .filter( (user) => user['name'] === name); 
 }
 
+const findUserById = (id) =>
+    users['users_list']
+        .find( (user) => user['id'] === id);
+
+        app.use(express.json());
+
+        app.get('/', (req, res) => {
+            res.send('Hello World!');
+        });
+        
+        app.listen(port, () => {
+            console.log(`Example app listening at http://localhost:${port}`);
+        });
+        
+        
+        app.get('/users', (req, res) => {
+            res.send(users);
+        });
+        
+//ex: http://localhost:8000/users/zap555
+app.get('/users/:id', (req, res) => {
+    const id = req.params['id']; //or req.params.id
+    let result = findUserById(id);
+    if (result === undefined) {
+        res.status(404).send('Resource not found.');
+    } else {
+        res.send(result);
+    }
+});
+
 app.get('/users', (req, res) => {
     const name = req.query.name;
     if (name != undefined){
@@ -49,18 +79,4 @@ app.get('/users', (req, res) => {
     }
 });
 
-app.use(express.json());
-
-app.get('/', (req, res) => {
-    res.send('Hello World!');
-});
-
-app.listen(port, () => {
-    console.log(`Example app listening at http://localhost:${port}`);
-});
-
-
-app.get('/users', (req, res) => {
-    res.send(users);
-});
 
